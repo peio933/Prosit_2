c5c910a470e4226bef530060b6d1610b5f33f2e9
Peio Rigaud <peio.rigaud@viacesi.fr>
2023-11-15 13:08:34 +0100

    fin du code mais probleme affichage

--- Prosit2/Prosit2.cpp
+++ Prosit2/Prosit2.cpp
@@ -9,7 +9,7 @@ int main()
 	File fileTool;
 	string text;
 	string txtACoder;
-	string txtDecoder;
+	string txtDecode;
 	int decalage;
 	char key;
 	int mode = -1;
@@ -29,29 +29,104 @@ int main()
 				cin >> modeCode;
 			}
 
-			cout << " Entrez le nom du fichier à crypter : " << endl;
+			cout << "Entrez le chemin d'accès du fichier à crypter :" << endl;
 			cin >> text;
 
 			int inputMode = 0;
-			cout << " Entrez le mode d'entrée :\n 1 - Texte\n 2 - Binaire" << endl;
+			cout << "Voulez-vous crypter à partir:\n1. Message saisi\n2. Fichier" << endl;
 			cin >> inputMode;
 
 			if (inputMode == 1)
 			{
-				cout << " Entrez le texte à crypter : " << endl;
+				cout << "Entrez le texte à crypter :" << endl;
 				cin >> txtACoder;
 			}
-			else if (inputMode == 2)
+
+			if (mode == 1 or mode == 3)
 			{
-				cout << " Entrez la taille du fichier à crypter : " << endl;
-				cin >> size;
-				cout << " Entrez le texte à crypter : " << endl;
-				cin >> txtACoder;
+				cout << "Entrez le décalage :" << endl;
+				cin >> decalage;
 
-			
+				if (modeCode == 2)
+				{
+					decalage = -decalage;
+				}
+
+				if (mode == 1)
+				{
+					if (inputMode == 1)
+					{
+						txtDecode = crypt.cesar(txtACoder, decalage);
+					}
+					else
+					{
+						txtDecode = crypt.cesar(fileTool.readFile(text + ".txt"), decalage);
+					}
+					fileTool.writeFile(text + ".txt", txtDecode);
+				}
+			}
+
+			if (mode == 2 or mode == 3)
+			{
+				cout << " Entrez la clé de chiffrement : " << endl;
+				cin >> key;
+				if (modeCode == 2)
+				{
+					cout << " Entrez la taille du fichier: " << endl;
+					cin >> size;
+
+				}
+				else
+				{
+					size = sizeof(txtACoder);
+				}
+				if (mode == 2)
+				{
+					if (inputMode == 1)
+					{
+						txtDecode = crypt.XOR(txtACoder, key);
+					}
+					else
+					{
+						txtDecode = crypt.XOR(fileTool.readFileBin(text + ".bin", size), key);
+					}
+					cout << txtDecode << endl;
+					if (modeCode == 1)
+					{
+						fileTool.writeFileBin(text + ".bin", txtDecode, size);
+					}
+					else
+					{
+						fileTool.writeFile(text + ".txt", txtDecode);
+					}
+				}
+			}
+			if (mode == 3)
+			{
+				if (inputMode == 1)
+				{
+					txtDecode = crypt.cesarXXOR(txtACoder, decalage, key, modeCode);
+				}
+				else
+				{
+					txtDecode = crypt.cesarXXOR(fileTool.readFileBin(text + ".bin", size), decalage, key, modeCode);
+				}
+
+				if (modeCode == 1)
+				{
+					fileTool.writeFileBin(text + ".bin", txtDecode, size);
+				}
+				else
+				{
+					cout << "Texte décodé: " << txtDecode << endl;
+					fileTool.writeFile(text + ".txt", txtDecode);
+				}
+
+			}
+			cout << "Fichier " << text << " crypté avec succès !" << endl;
 		}
-
 	}
 
 }
+
 
